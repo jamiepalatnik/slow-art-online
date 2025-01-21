1cc53e2df89c0cd4a2ce6c88605760a5af59c72d
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-01-21 15:49:58 -0500

    Get primary image from API

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import requests
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
-
+    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/437869"
+    artwork = requests.get(url)
+    image_data = artwork.json()
+    img = image_data["primaryImage"]
+    return render_template("index.html", img=img)
 
 if __name__ == "__main__":
     main()
