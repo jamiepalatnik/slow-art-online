4f6feb018aac39a22d66d2547c8f57bb4e69d216
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-01-21 15:50:43 -0500

    Rename variable

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def main():
     url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/437869"
     artwork = requests.get(url)
-    image_data = artwork.json()
-    img = image_data["primaryImage"]
+    artwork_data = artwork.json()
+    img = artwork_data["primaryImage"]
     return render_template("index.html", img=img)
 
 if __name__ == "__main__":
