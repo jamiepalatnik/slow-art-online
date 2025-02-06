bbb440eb15222236882631b0cae39b1e971171f7
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-02-05 21:20:44 -0500

    Refactor code to handle HTTPError

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import requests
+import sys
 
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+
+    # Get data from the Met API
     url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/437869"
-    artwork = requests.get(url)
-    artwork_data = artwork.json()
-    img = artwork_data["primaryImage"]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.HTTPError:
+        print("Could not complete request.")
+        sys.exit(1)
+
+    content = response.json()
+    img = content["primaryImage"]
     return render_template("index.html", img=img)
 
 @app.route("/about")
