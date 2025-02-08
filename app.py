eaf0c364f567e2d6cac965ca2b35b1c8b9d7211d
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-02-08 14:48:55 -0500

    Add route and function to get random image

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import random
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def main():
 
     # Get data from the Met API
-    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/437869"
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/437869"
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -17,8 +18,98 @@ def main():
         sys.exit(1)
 
     content = response.json()
-    img = content["primaryImage"]
-    return render_template("index.html", img=img)
+    image = content["primaryImage"]
+
+    return render_template("index.html", image=image)
+
+# Get random image from the Met API
+@app.route('/random-image')
+def get_random_image():
+    pass
+    # # Get a list of object IDs that are highlights and have images
+    # try:
+    #     response = requests.get(
+    #         "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=1&q=painting"
+    #     )
+    #     response.raise_for_status()
+    # except requests.HTTPError:
+    #     print("Couldn't complete request!")
+    #     sys.exit(1)
+
+    # content = response.json()
+    # print(content)
+    
+    # # Get list from JSON
+    # highlights = content["objectIDs"]
+    # print(highlights)
+
+    # Random selection from American Wing, not filtered
+    highlights = [
+    701989,
+    20517,
+    11797,
+    11130,
+    11116,
+    12828,
+    14930,
+    10813,
+    10080,
+    11981,
+    13065,
+    19261,
+    11227,
+    13171,
+    16687,
+    11050,
+    12600,
+    16584,
+    10497,
+    11605,
+    11122,
+    10482,
+    11133,
+    11145,
+    13052,
+    10388,
+    12822,
+    10159,
+    11619,
+    11734,
+    14472,
+    10819,
+    694642,
+    20888,
+    20768,
+    10557,
+    11707,
+    11396,
+    11417,
+    10786,
+    10554,
+    10186,
+    19346,
+    12127,
+    11263,
+    11269,
+    11311,
+    ]
+
+    # Generate object request using random object ID from list
+    random_objectID = str(random.choice(highlights))
+
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + random_objectID
+
+    try:
+        objectResponse = requests.get(url)
+        objectResponse.raise_for_status()
+    except requests.HTTPError:
+        print("Could not complete request.")
+        sys.exit(1)
+
+    objectContent = objectResponse.json()
+    random_image = objectContent["primaryImage"]
+
+    return render_template("random-image.html", random_image=random_image)
 
 @app.route("/about")
 def about():
