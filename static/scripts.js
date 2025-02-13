a330c6eaa19e359e8a250cc55ff607bc197c8ac8
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-02-13 15:18:22 -0500

    Rewrite timer function to use requestAnimationFrame

--- static/scripts.js
+++ static/scripts.js
@@ -25,38 +25,55 @@ function closePanel() {
 
 // TODO: Timer
 
-// Define timer button
+// Define timer function and variables
 const timerButton = document.getElementById('timerButton');
+let timerState = "stopped";
 
+// Set the timer to 5 minutes (1 minute for testing)
+let minutes = 5;
+let duration = minutes * 60 * 1000; // Duration in milliseconds
+let startTime = Date.now();
+let endTime = startTime + duration;
+let pausedTime;
+
+// Define timer countdown function
+function updateTimer() {
+
+    let currentTime = Date.now();
+    let remainingTime = endTime - currentTime;
+    let remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    let remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+    // Add 0 to seconds display when there are less than 10 seconds remaining
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+
+    if (remainingTime <= 0) {
+        timerState = "stopped";
+        document.getElementById("timerDisplay").innerHTML = "Done";
+        return;
+    }
+
+    // Update button text and display the remaining time
+    document.getElementById("timerDisplay").innerHTML = remainingMinutes + ":" + remainingSeconds;
+
+    // Update timer button text depending on timer state
+    if (timerState === "running") {
+        requestAnimationFrame(updateTimer);
+        document.getElementById("timerButtonText").innerHTML = "Pause timer";
+    } else if (timerState === "stopped" || timerState === "paused") {
+        document.getElementById("timerButtonText").innerHTML = "Start timer";
+    }
+}
+
+// When timer button is clicked, update timer state depending on current timer state
 timerButton.onclick = function() {
-
-    // Set the timer to 5 minutes
-    let minutes = 5;
-    let duration = minutes * 60 * 1000; // Duration in milliseconds
-    let startTime = Date.now();
-    let endTime = startTime + duration;
-
-    function updateTimer() {
-        let currentTime = Date.now();
-        let remainingTime = endTime - currentTime;
-      
-        if (remainingTime <= 0) {
-          clearInterval(timerInterval);
-          document.getElementById("timerDisplay").innerHTML = "Done";
-          return;
-        }
-      
-        let remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-        let remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-
-        // Add 0 to seconds display when there are less than 10 seconds remaining
-        if (remainingSeconds < 10) {
-            remainingSeconds = "0" + remainingSeconds
-        }
-      
-        // Display the remaining time
-        document.getElementById("timerDisplay").innerHTML = remainingMinutes + ":" + remainingSeconds;
-      }
-      
-      let timerInterval = setInterval(updateTimer, 1000);
-} 
+    if (timerState === "stopped" || timerState === "paused") {
+        timerState = "running";
+        requestAnimationFrame(updateTimer);
+    } else if (timerState === "running") {
+        timerState = "paused";
+        // TODO Store elapsed time in a variable so when the timer restarts it can restart where it left off
+    }
+}
